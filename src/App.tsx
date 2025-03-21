9514401d109167718301736d95104acf74a3440a
Paul Peters <paul.peters@waofintech.com>
2025-03-21 09:36:56 -0500

    feat: added api req

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import { ComboboxDemo } from './components/example-combobox'
 import { DialogDemo } from './components/example-dialog'
 import viteLogo from '/vite.svg'
+import { fetchTodos, Todo } from './api/api';
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleFetchTodos = async () => {
+    const todos = await fetchTodos();
+    setTodos(todos);
+  };
+
   return (
     <div className=''>
       <div className="flex justify-center space-x-4 mt-24">
@@ -14,11 +22,23 @@ function App() {
           <img src={reactLogo} className="h-16" alt="React logo" />
         </a>
       </div>
-      <h1 className="text-3xl font-bold text-center mt-4">Vite + React</h1>
 
-      <p className="read-the-docs text-center mt-4">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='mt-12 w-full flex justify-center'>
+        <button onClick={handleFetchTodos} className="px-4 py-2 bg-blue-500 text-white rounded">
+          Fetch Todos
+        </button>
+      </div>
+
+      {todos.length > 0 && (
+        <div className='mt-12 w-full flex flex-col items-center'>
+          {todos.map(todo => (
+            <div key={todo.id} className="p-4 border-b w-full max-w-md">
+              <h2 className="text-xl font-bold">{todo.title}</h2>
+              <p>Completed: {todo.completed ? 'Yes' : 'No'}</p>
+            </div>
+          ))}
+        </div>
+      )}
 
       <div className='w-full mt-12 flex justify-center'>
         <DialogDemo />
@@ -29,7 +49,7 @@ function App() {
       </div>
 
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/api/api.ts
@@ -0,0 +1,18 @@
+export interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+export const fetchTodos = async (): Promise<Todo[]> => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch records");
+    }
+
+    const json = response.json();
+
+    return json.slice(0, 10)
+};
